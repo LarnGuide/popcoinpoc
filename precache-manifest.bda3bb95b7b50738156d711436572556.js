a19a8bbebb747b501f6fa21f94c95ce593ce796a
user <user@email.com>
2022-03-01 10:13:07 +0000

    deploy

---
+++ precache-manifest.bda3bb95b7b50738156d711436572556.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf27b1d4eabec8852c97d116abb06ec",
+    "url": "/popcoinpoc/404.html"
+  },
+  {
+    "revision": "10c7497451338e39ebdee3e300f3f6ff",
+    "url": "/popcoinpoc/controllers/HtmlSanitizer.js"
+  },
+  {
+    "revision": "4bc8f50033b98258d2ec",
+    "url": "/popcoinpoc/css/chunk-vendors.4ead101a.css"
+  },
+  {
+    "revision": "5c07f189b697e88bcf61",
+    "url": "/popcoinpoc/css/index.7746f33a.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/popcoinpoc/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/popcoinpoc/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/popcoinpoc/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/popcoinpoc/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/popcoinpoc/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/popcoinpoc/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "e04efa7527460e6d38b1ba79b405761b",
+    "url": "/popcoinpoc/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.e04efa75.woff"
+  },
+  {
+    "revision": "3e82d9a3cad4ab32b96d5589d55db0e6",
+    "url": "/popcoinpoc/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.3e82d9a3.woff2"
+  },
+  {
+    "revision": "ff076f222c030b7ec40b7372aa008746",
+    "url": "/popcoinpoc/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "22d271afcd4d6ad3c3ef73ee82a52155",
+    "url": "/popcoinpoc/img/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "e306267e1731bf56a99f9628384a72ce",
+    "url": "/popcoinpoc/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "920d92e685404fac3cd4f8c2081fb62c",
+    "url": "/popcoinpoc/img/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "734e1a4b243d49dd3982fb3afe23d6d4",
+    "url": "/popcoinpoc/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "0ccb61711de8799d82d2effc316a7a35",
+    "url": "/popcoinpoc/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "9dd7c15cec7633e170f21e7d9aefb1a0",
+    "url": "/popcoinpoc/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "606e7a747b1415c2dca7e1d94027aa4a",
+    "url": "/popcoinpoc/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "e507b31be727cd9c77e38ce98212cf27",
+    "url": "/popcoinpoc/img/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "677c8a04d08e24aa9692bddf8b116115",
+    "url": "/popcoinpoc/img/splashscreens/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "d0a443bc7a156b0f694ad106f5ab9e63",
+    "url": "/popcoinpoc/index.html"
+  },
+  {
+    "revision": "4bc8f50033b98258d2ec",
+    "url": "/popcoinpoc/js/chunk-vendors.92df25e5.js"
+  },
+  {
+    "revision": "5c07f189b697e88bcf61",
+    "url": "/popcoinpoc/js/index.b1cfef8a.js"
+  },
+  {
+    "revision": "253814c30b9450e9f4bc770c7c193727",
+    "url": "/popcoinpoc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/popcoinpoc/robots.txt"
+  }
+]);
